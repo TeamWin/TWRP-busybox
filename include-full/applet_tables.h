dfd4156f8cab9f40a8024828e9519830c9f98c7b
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-07-05 20:03:50 +0200

    restore default config for first build after repo init or repo sync

---
+++ include-full/applet_tables.h
@@ -0,0 +1,638 @@
+/* This is a generated file, don't edit */
+
+#define NUM_APPLETS 207
+
+const char applet_names[] ALIGN1 = ""
+"[" "\0"
+"[[" "\0"
+"arp" "\0"
+"ash" "\0"
+"awk" "\0"
+"base64" "\0"
+"basename" "\0"
+"bbconfig" "\0"
+"blockdev" "\0"
+"brctl" "\0"
+"bunzip2" "\0"
+"bzcat" "\0"
+"bzip2" "\0"
+"cal" "\0"
+"cat" "\0"
+"catv" "\0"
+"chattr" "\0"
+"chgrp" "\0"
+"chmod" "\0"
+"chown" "\0"
+"chroot" "\0"
+"clear" "\0"
+"cmp" "\0"
+"comm" "\0"
+"cp" "\0"
+"cpio" "\0"
+"crond" "\0"
+"crontab" "\0"
+"cut" "\0"
+"date" "\0"
+"dc" "\0"
+"dd" "\0"
+"depmod" "\0"
+"devmem" "\0"
+"df" "\0"
+"diff" "\0"
+"dirname" "\0"
+"dmesg" "\0"
+"dnsd" "\0"
+"dos2unix" "\0"
+"du" "\0"
+"echo" "\0"
+"ed" "\0"
+"egrep" "\0"
+"env" "\0"
+"expand" "\0"
+"expr" "\0"
+"false" "\0"
+"fdisk" "\0"
+"fgrep" "\0"
+"find" "\0"
+"flock" "\0"
+"fold" "\0"
+"free" "\0"
+"freeramdisk" "\0"
+"fsck" "\0"
+"fsync" "\0"
+"ftpget" "\0"
+"ftpput" "\0"
+"fuser" "\0"
+"getopt" "\0"
+"grep" "\0"
+"groups" "\0"
+"gunzip" "\0"
+"gzip" "\0"
+"halt" "\0"
+"head" "\0"
+"hexdump" "\0"
+"id" "\0"
+"ifconfig" "\0"
+"insmod" "\0"
+"install" "\0"
+"iostat" "\0"
+"ip" "\0"
+"kill" "\0"
+"killall" "\0"
+"killall5" "\0"
+"less" "\0"
+"ln" "\0"
+"losetup" "\0"
+"ls" "\0"
+"lsattr" "\0"
+"lsmod" "\0"
+"lsusb" "\0"
+"lzcat" "\0"
+"lzma" "\0"
+"lzop" "\0"
+"lzopcat" "\0"
+"md5sum" "\0"
+"mkdir" "\0"
+"mkdosfs" "\0"
+"mke2fs" "\0"
+"mkfifo" "\0"
+"mkfs.ext2" "\0"
+"mkfs.vfat" "\0"
+"mknod" "\0"
+"mkswap" "\0"
+"mktemp" "\0"
+"modinfo" "\0"
+"modprobe" "\0"
+"more" "\0"
+"mount" "\0"
+"mountpoint" "\0"
+"mpstat" "\0"
+"mv" "\0"
+"nanddump" "\0"
+"nandwrite" "\0"
+"nbd-client" "\0"
+"nc" "\0"
+"netstat" "\0"
+"nice" "\0"
+"nohup" "\0"
+"nslookup" "\0"
+"ntpd" "\0"
+"od" "\0"
+"patch" "\0"
+"pgrep" "\0"
+"pidof" "\0"
+"ping" "\0"
+"pipe_progress" "\0"
+"pkill" "\0"
+"pmap" "\0"
+"poweroff" "\0"
+"printenv" "\0"
+"printf" "\0"
+"ps" "\0"
+"pstree" "\0"
+"pwd" "\0"
+"pwdx" "\0"
+"rdev" "\0"
+"readlink" "\0"
+"realpath" "\0"
+"reboot" "\0"
+"renice" "\0"
+"reset" "\0"
+"resize" "\0"
+"rev" "\0"
+"rm" "\0"
+"rmdir" "\0"
+"rmmod" "\0"
+"route" "\0"
+"run-parts" "\0"
+"rx" "\0"
+"sed" "\0"
+"seq" "\0"
+"setconsole" "\0"
+"setserial" "\0"
+"setsid" "\0"
+"sh" "\0"
+"sha1sum" "\0"
+"sha256sum" "\0"
+"sha512sum" "\0"
+"sleep" "\0"
+"sort" "\0"
+"split" "\0"
+"stat" "\0"
+"strings" "\0"
+"stty" "\0"
+"sum" "\0"
+"swapoff" "\0"
+"swapon" "\0"
+"switch_root" "\0"
+"sync" "\0"
+"sysctl" "\0"
+"tac" "\0"
+"tail" "\0"
+"tar" "\0"
+"tee" "\0"
+"telnet" "\0"
+"telnetd" "\0"
+"test" "\0"
+"tftp" "\0"
+"tftpd" "\0"
+"time" "\0"
+"timeout" "\0"
+"top" "\0"
+"touch" "\0"
+"tr" "\0"
+"traceroute" "\0"
+"true" "\0"
+"tty" "\0"
+"tune2fs" "\0"
+"umount" "\0"
+"uname" "\0"
+"uncompress" "\0"
+"unexpand" "\0"
+"uniq" "\0"
+"unix2dos" "\0"
+"unlzma" "\0"
+"unlzop" "\0"
+"unxz" "\0"
+"unzip" "\0"
+"uptime" "\0"
+"usleep" "\0"
+"uudecode" "\0"
+"uuencode" "\0"
+"vi" "\0"
+"watch" "\0"
+"wc" "\0"
+"wget" "\0"
+"which" "\0"
+"whoami" "\0"
+"xargs" "\0"
+"xz" "\0"
+"xzcat" "\0"
+"yes" "\0"
+"zcat" "\0"
+;
+
+#ifndef SKIP_applet_main
+int (*const applet_main[])(int argc, char **argv) = {
+test_main,
+test_main,
+arp_main,
+ash_main,
+awk_main,
+base64_main,
+basename_main,
+bbconfig_main,
+blockdev_main,
+brctl_main,
+bunzip2_main,
+bunzip2_main,
+bzip2_main,
+cal_main,
+cat_main,
+catv_main,
+chattr_main,
+chgrp_main,
+chmod_main,
+chown_main,
+chroot_main,
+clear_main,
+cmp_main,
+comm_main,
+cp_main,
+cpio_main,
+crond_main,
+crontab_main,
+cut_main,
+date_main,
+dc_main,
+dd_main,
+depmod_main,
+devmem_main,
+df_main,
+diff_main,
+dirname_main,
+dmesg_main,
+dnsd_main,
+dos2unix_main,
+du_main,
+echo_main,
+ed_main,
+grep_main,
+env_main,
+expand_main,
+expr_main,
+false_main,
+fdisk_main,
+grep_main,
+find_main,
+flock_main,
+fold_main,
+free_main,
+freeramdisk_main,
+fsck_main,
+fsync_main,
+ftpgetput_main,
+ftpgetput_main,
+fuser_main,
+getopt_main,
+grep_main,
+id_main,
+gunzip_main,
+gzip_main,
+halt_main,
+head_main,
+hexdump_main,
+id_main,
+ifconfig_main,
+insmod_main,
+install_main,
+iostat_main,
+ip_main,
+kill_main,
+kill_main,
+kill_main,
+less_main,
+ln_main,
+losetup_main,
+ls_main,
+lsattr_main,
+lsmod_main,
+lsusb_main,
+unlzma_main,
+unlzma_main,
+lzop_main,
+lzop_main,
+md5_sha1_sum_main,
+mkdir_main,
+mkfs_vfat_main,
+mkfs_ext2_main,
+mkfifo_main,
+mkfs_ext2_main,
+mkfs_vfat_main,
+mknod_main,
+mkswap_main,
+mktemp_main,
+modinfo_main,
+modprobe_main,
+more_main,
+mount_main,
+mountpoint_main,
+mpstat_main,
+mv_main,
+nandwrite_main,
+nandwrite_main,
+nbdclient_main,
+nc_main,
+netstat_main,
+nice_main,
+nohup_main,
+nslookup_main,
+ntpd_main,
+od_main,
+patch_main,
+pgrep_main,
+pidof_main,
+ping_main,
+pipe_progress_main,
+pgrep_main,
+pmap_main,
+halt_main,
+printenv_main,
+printf_main,
+ps_main,
+pstree_main,
+pwd_main,
+pwdx_main,
+rdev_main,
+readlink_main,
+realpath_main,
+halt_main,
+renice_main,
+reset_main,
+resize_main,
+rev_main,
+rm_main,
+rmdir_main,
+rmmod_main,
+route_main,
+run_parts_main,
+rx_main,
+sed_main,
+seq_main,
+setconsole_main,
+setserial_main,
+setsid_main,
+ash_main,
+md5_sha1_sum_main,
+md5_sha1_sum_main,
+md5_sha1_sum_main,
+sleep_main,
+sort_main,
+split_main,
+stat_main,
+strings_main,
+stty_main,
+sum_main,
+swap_on_off_main,
+swap_on_off_main,
+switch_root_main,
+sync_main,
+sysctl_main,
+tac_main,
+tail_main,
+tar_main,
+tee_main,
+telnet_main,
+telnetd_main,
+test_main,
+tftp_main,
+tftpd_main,
+time_main,
+timeout_main,
+top_main,
+touch_main,
+tr_main,
+traceroute_main,
+true_main,
+tty_main,
+tune2fs_main,
+umount_main,
+uname_main,
+uncompress_main,
+expand_main,
+uniq_main,
+dos2unix_main,
+unlzma_main,
+lzop_main,
+unxz_main,
+unzip_main,
+uptime_main,
+usleep_main,
+uudecode_main,
+uuencode_main,
+vi_main,
+watch_main,
+wc_main,
+wget_main,
+which_main,
+whoami_main,
+xargs_main,
+unxz_main,
+unxz_main,
+yes_main,
+gunzip_main,
+};
+#endif
+
+const uint16_t applet_nameofs[] ALIGN2 = {
+0x0000,
+0x0002,
+0x0005,
+0x0009,
+0x000d,
+0x0011,
+0x0018,
+0x0021,
+0x002a,
+0x0033,
+0x0039,
+0x0041,
+0x0047,
+0x004d,
+0x0051,
+0x0055,
+0x005a,
+0x0061,
+0x0067,
+0x006d,
+0x0073,
+0x007a,
+0x0080,
+0x0084,
+0x0089,
+0x008c,
+0x0091,
+0x8097,
+0x009f,
+0x00a3,
+0x00a8,
+0x00ab,
+0x00ae,
+0x00b5,
+0x00bc,
+0x00bf,
+0x00c4,
+0x00cc,
+0x00d2,
+0x00d7,
+0x00e0,
+0x00e3,
+0x00e8,
+0x00eb,
+0x00f1,
+0x00f5,
+0x00fc,
+0x0101,
+0x0107,
+0x010d,
+0x0113,
+0x0118,
+0x011e,
+0x0123,
+0x0128,
+0x0134,
+0x0139,
+0x013f,
+0x0146,
+0x014d,
+0x0153,
+0x015a,
+0x015f,
+0x0166,
+0x016d,
+0x0172,
+0x0177,
+0x017c,
+0x0184,
+0x0187,
+0x0190,
+0x0197,
+0x019f,
+0x01a6,
+0x01a9,
+0x01ae,
+0x01b6,
+0x01bf,
+0x01c4,
+0x01c7,
+0x01cf,
+0x01d2,
+0x01d9,
+0x01df,
+0x01e5,
+0x01eb,
+0x01f0,
+0x01f5,
+0x01fd,
+0x0204,
+0x020a,
+0x0212,
+0x0219,
+0x0220,
+0x022a,
+0x0234,
+0x023a,
+0x0241,
+0x0248,
+0x0250,
+0x0259,
+0x025e,
+0x0264,
+0x026f,
+0x0276,
+0x0279,
+0x0282,
+0x028c,
+0x0297,
+0x029a,
+0x02a2,
+0x02a7,
+0x02ad,
+0x02b6,
+0x02bb,
+0x02be,
+0x02c4,
+0x02ca,
+0x42d0,
+0x02d5,
+0x02e3,
+0x02e9,
+0x02ee,
+0x02f7,
+0x0300,
+0x0307,
+0x030a,
+0x0311,
+0x0315,
+0x031a,
+0x031f,
+0x0328,
+0x0331,
+0x0338,
+0x033f,
+0x0345,
+0x034c,
+0x0350,
+0x0353,
+0x0359,
+0x035f,
+0x0365,
+0x036f,
+0x0372,
+0x0376,
+0x037a,
+0x0385,
+0x038f,
+0x0396,
+0x0399,
+0x03a1,
+0x03ab,
+0x03b5,
+0x03bb,
+0x03c0,
+0x03c6,
+0x03cb,
+0x03d3,
+0x03d8,
+0x03dc,
+0x03e4,
+0x03eb,
+0x03f7,
+0x03fc,
+0x0403,
+0x0407,
+0x040c,
+0x0410,
+0x0414,
+0x041b,
+0x0423,
+0x0428,
+0x042d,
+0x0433,
+0x0438,
+0x0440,
+0x0444,
+0x044a,
+0x444d,
+0x0458,
+0x045d,
+0x0461,
+0x0469,
+0x0470,
+0x0476,
+0x0481,
+0x048a,
+0x048f,
+0x0498,
+0x049f,
+0x04a6,
+0x04ab,
+0x04b1,
+0x04b8,
+0x04bf,
+0x04c8,
+0x04d1,
+0x04d4,
+0x04da,
+0x04dd,
+0x04e2,
+0x04e8,
+0x04ef,
+0x04f5,
+0x04f8,
+0x04fe,
+0x0502,
+};
+
+
+#define MAX_APPLET_NAME_LEN 13
